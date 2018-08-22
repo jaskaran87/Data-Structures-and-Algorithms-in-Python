d1172b1df82e0dbf306d46d2b301d9c8fc89e20f
jaskaran87 <jaskaransinghrajal@gmail.com>
2018-08-22 23:31:45 +0530

    imporve commenting in selection sort

--- sorting-algorithms/selection_sort.py
+++ sorting-algorithms/selection_sort.py
@@ -1,8 +1,8 @@
 # Python program for implementation of Selection Sort
 
-# select sort is fast then bubble sort because 
-# In bubble sort we have write down on every iteration if condition is satisfy
-# If we write down any thing in memory then it take time
+# Selection sort is fast then bubble sort because 
+# In bubble sort, We have to write down on every iteration if condition is satisfy
+# If we write down any thing in memory again and again then it take time
 # but in selection sort we have to write down only one time end of first loop
 
 A = [50,4,21,2,9,90,6]
