0da3a09c419846477582a5b709f4cbed4a84500e
Jaskaran singh <jaskaran.select@logixgrid.com>
2018-12-24 09:51:44 +0530

    Property Decorators

---
+++ Core/property_decorators.py
@@ -0,0 +1,35 @@
+class Employer:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        
+    @property
+    def email(self):
+        return ('{first}.{last}@email.com'.format(first = self.first,last = self.last))
+
+    @property
+    def fullname(self):
+        return ('{first} {last}'.format(last = self.last, first = self.first))
+
+    @fullname.setter
+    def fullname(self, name):
+        self.first, self.last = name.split(' ')
+
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete fullname')
+        self.first = None
+        self.last = None
+
+
+emp_1 = Employer('jaskaran', 'singh')
+print("Frist Name: {first} ".format(first = emp_1.first))
+print("last Name: {last}".format(last = emp_1.last))
+print(emp_1.email)
+
+emp_1.fullname = 'Amandeep singh'
+
+print(emp_1.fullname)
+
+del emp_1.fullname
