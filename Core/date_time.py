cc4d4febedeb9ba3eae06ed3a5e8279b73000e25
Jaskaran singh <jaskaran.select@logixgrid.com>
2019-07-01 11:47:29 +0530

    date and ftp connection with python

--- Core/date_time.py
+++ Core/date_time.py
@@ -2,6 +2,7 @@ import datetime
 from datetime import  timedelta, datetime
 print((datetime.now() + timedelta(hours=5, minutes=30)).replace(hour=0, minute=0, second=0, microsecond=0))
 #today date
+current_date = datetime(2011, 07,07)
 date = datetime.today()
 date
 print(date)
@@ -31,5 +32,6 @@ sec =(totsec%3600)%60 #just for reference
 print "%d:%d" %(h,m)
 
 
-date_time = datetime.datetime.now()
+date_time = datetime.now()
 date = date_time.day + date_time.month + date_time.year  + date_time.hour + date_time.second + date_time.minute + date_time.microsecond
+print(date)
--- Core/my_sql/my_sql_question.py
+++ Core/my_sql/my_sql_question.py
@@ -1,1 +1,26 @@
 https://stackoverflow.com/questions/6107167/mysql-delete-duplicate-records-but-keep-latest
+
+table employer
+name
+code
+address
+state 
+city
+ID
+
+Attendance
+employer_code
+Attendance_date
+status (present, absent, leave)
+ID
+
+
+
+
+1) GET count of employer table city wise
+
+2) write a query that return duplicate employer name
+
+3) write query that return all records of odd id records from emp table
+
+4) give count a leave table by employer A in month of June 18
